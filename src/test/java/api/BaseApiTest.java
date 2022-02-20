976951efc43f17e7030651ee9e94ca9bd3bde674
sunny <5olnce@mail.ru>
2022-02-20 22:03:55 +0200

    change BaseApiTest

--- src/test/java/api/BaseApiTest.java
+++ src/test/java/api/BaseApiTest.java
@@ -1,14 +1,43 @@
 package api;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import helpers.Attach;
 import io.restassured.RestAssured;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import ui.BaseTest;
 
 import static config.ProjectConfig.PROJECT_CONFIG;
+import static io.qameta.allure.Allure.step;
 
 public class BaseApiTest extends BaseTest {
+    @SneakyThrows
     @BeforeAll
-    public static void before() {
-        RestAssured.baseURI = PROJECT_CONFIG.baseUrl();
+    static void before() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        if (!PROJECT_CONFIG.localRun()) {
+            Configuration.remote = PROJECT_CONFIG.removeUrl();
+            capabilities.setCapability("browserName", PROJECT_CONFIG.browser());
+            capabilities.setCapability("browserVersion", PROJECT_CONFIG.browserVersion());
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+        }
+
+        Configuration.browserSize = PROJECT_CONFIG.browserSize();
+        Configuration.browserCapabilities = capabilities;
     }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        step("Закрыть браузер", Selenide::closeWebDriver);
+    }
+
 }
